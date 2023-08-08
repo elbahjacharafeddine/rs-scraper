49373fdafd508501bef3117ef608a0acf3685e4a
elbahjacharafeddine <101811374+elbahjacharafeddine@users.noreply.github.com>
2023-08-08 18:11:55 +0100

    update commit SJR

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ wss.on('connection', async (ws) => {
                 console.log('navigation to scopus...')
 
                 await page.waitForTimeout(1500)
-               await page.waitForSelector('#scopus-author-profile-page-control-microui__general-information-content', {timeout: 4000});
+               // await page.waitForSelector('#scopus-author-profile-page-control-microui__general-information-content', {timeout: 4000});
 
                 const name = await page.$eval('#scopus-author-profile-page-control-microui__general-information-content > div.Col-module__hwM1N.offset-lg-2 > div > h1 > strong', (e) => e.textContent.trim().replace(',', ''))
                 const univer = await page.$eval('#scopus-author-profile-page-control-microui__general-information-content > div.Col-module__hwM1N.offset-lg-2 > ul > li.AuthorHeader-module__DRxsE > span > a > span.Typography-module__lVnit.Typography-module__Nfgvc.Button-module__Imdmt', (e) => e.textContent.trim())
